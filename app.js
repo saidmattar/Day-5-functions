93c251443950872701ccaa206ac4688d04c2557c
saidmattar <ssabdou@gmail.com>
2017-04-07 17:44:39 -0700

    multarray-fun-q5

--- app.js
+++ app.js
@@ -87,10 +87,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(testArray){ //eslint-disable-line
-
+var total = 1;
+for (var i = 0; i < testArray.length; i++) {
+total = multiply(total, testArray[i])[0];
+}
+return[total,'The numbers ' + testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' have a product of ' + total + '.'];
 }
 
-// Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+//Here is the test for multiplyArray(); uncomment it to run it
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
